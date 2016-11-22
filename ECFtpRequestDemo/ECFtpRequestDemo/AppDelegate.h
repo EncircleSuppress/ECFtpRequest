9df9a9cafede369bcd0f1c986a96ddb1222e9c6b
EncircleSuppress <xiaofanyu@163.com>
2016-11-22 21:01:51 +0800

    ..

---
+++ ECFtpRequestDemo/ECFtpRequestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ECFtpRequestDemo
+//
+//  Created by mxc235 on 2016/11/22.
+//  Copyright © 2016年 EncircleCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ECFtpRequestDemo/ECFtpRequestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECFtpRequestDemo
+//
+//  Created by mxc235 on 2016/11/22.
+//  Copyright © 2016年 EncircleCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
